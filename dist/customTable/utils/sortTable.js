8802da7f198e4ad88bb7c0017c9af6ae7a4b2c9d
Tanisha060802 <125466934+Tanisha060802@users.noreply.github.com>
2025-01-27 11:54:18 +0530

    sortTable.js

--- dist/customTable/utils/sortTable.js
+++ dist/customTable/utils/sortTable.js
@@ -4,20 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = sortTable;
-/**
- *
- * Orders a table according to the values of a specific field of the table
- *
- * @function sortTable
- *
- * @param table is the input data that needs to be ordered, it has to be an array of objects
- * @param sortOrder can take "asc" or "desc" to define the sorting order
- * @param sortField the field that needs to be used for sorting (example : firstName)
- *
- * @returns a new table with objects that are sortered according to the params
- *
- */
-
 function sortTable(table, sortOrder, sortField) {
   var data = table;
   var reversed = sortOrder === 'asc' ? 1 : -1;
