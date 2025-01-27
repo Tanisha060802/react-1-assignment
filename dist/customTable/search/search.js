80fdf36025cfbf8aba84f77413cb5a0ed9142c92
Tanisha060802 <125466934+Tanisha060802@users.noreply.github.com>
2025-01-27 11:51:48 +0530

    search.js

--- dist/customTable/search/search.js
+++ dist/customTable/search/search.js
@@ -6,16 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = Search;
 var _react = _interopRequireDefault(require("react"));
-/**
- *
- * The Search bar part of the custom table component
- *
- * @function Search
- *
- * @returns the Search component
- *
- */
-
 function Search(_ref) {
   var onSearch = _ref.onSearch;
   return /*#__PURE__*/_react.default.createElement("input", {
